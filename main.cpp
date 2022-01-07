05619a235b1170ad307f3b376af414c88194986f
Ésia Jales <94870002+esiajales@users.noreply.github.com>
2022-01-07 15:39:22 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Sculptor.h"
+using namespace std;
+
+int main(void){
+  Sculptor ufo(57,57,57);
+
+  ufo.setColor(0.3, 0.3, 0.5, 1.0); // seta a cor pra cinza
+  ufo.putEllipsoid(25, 25, 25, 4, 25, 25); // faz a elipse (painel de controle)
+
+  ufo.setColor(0.5, 0.5, 0.5, 1.0); // seta a cor pra azul claro
+  ufo.putSphere(31, 25, 25, 16); // faz a esfera 
+
+  ufo.setColor(1.0, 1.0, 1.0, 1.0); // seta a cor para branco
+  ufo.putBox(47, 51, 24, 26, 24, 26); // faz a antena
+  ufo.putBox(51, 52, 23, 27, 23, 27); // detalhe da antena
+
+  ufo.setColor(0, 0, 0, 1.0); // seta a cor para preto
+  ufo.putBox(14, 23, 23, 25, 43, 45); // faz a primeira perna
+  ufo.putBox(13, 14, 22, 26, 42, 46); // detalhe da primeira perna
+
+  ufo.setColor(0, 0, 0, 0); // seta a cor para preto
+  ufo.putBox(14, 23, 23, 25, 6, 8); // faz a segunda perna
+  ufo.putBox(13, 14, 22, 26, 5, 9); // detalhe da segunda perna
+
+  ufo.setColor(0, 0, 0, 1.0); // seta a cor para preto
+  ufo.putBox(14, 23, 43, 45, 23, 25); // faz a terceira perna
+  ufo.putBox(13, 14, 42, 46, 22, 26); // detalhe da terceira perna
+  
+  ufo.writeOFF((char*)"ufo.off");
+  return 0;
+}
