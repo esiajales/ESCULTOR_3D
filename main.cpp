986f8311e3efd812eef4b5fe1adb0b13305a011b
Ésia Jales <94870002+esiajales@users.noreply.github.com>
2022-01-11 15:12:24 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(void){
   Sculptor ufo(57,57,57);
 
-  ufo.setColor(0.3, 0.3, 0.5, 1.0); // seta a cor pra cinza
+  ufo.setColor(0.3, 0.3, 0.5, 1.0); // seta a cor pra azul claro
   ufo.putEllipsoid(25, 25, 25, 4, 25, 25); // faz a elipse (painel de controle)
 
-  ufo.setColor(0.5, 0.5, 0.5, 1.0); // seta a cor pra azul claro
+  ufo.setColor(0.5, 0.5, 0.5, 1.0); // seta a cor pra cinza
   ufo.putSphere(31, 25, 25, 16); // faz a esfera 
 
   ufo.setColor(1.0, 1.0, 1.0, 1.0); // seta a cor para branco
@@ -19,7 +19,7 @@ int main(void){
   ufo.putBox(14, 23, 23, 25, 43, 45); // faz a primeira perna
   ufo.putBox(13, 14, 22, 26, 42, 46); // detalhe da primeira perna
 
-  ufo.setColor(0, 0, 0, 0); // seta a cor para preto
+  ufo.setColor(0, 0, 0, 1.0); // seta a cor para preto
   ufo.putBox(14, 23, 23, 25, 6, 8); // faz a segunda perna
   ufo.putBox(13, 14, 22, 26, 5, 9); // detalhe da segunda perna
 
